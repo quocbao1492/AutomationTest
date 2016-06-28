2deec9b02b4a9f3c427024e8e77ff469ebac6ccd
quocbao1492 <baottq.uit@gmail.com>
2016-06-28 15:54:41 +0700

    file changed

--- GithubTest/Test.py
+++ GithubTest/Test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(executable_path="C:\Test\GithubTest\chromedriver.exe")
+driver = webdriver.Firefox()
 
 driver.get("http://tinhte.vn")
 driver.find_element_by_xpath("//*[@id='navigation']/div/div/div/nav/div/div/ul[2]/li[1]/a").click()
