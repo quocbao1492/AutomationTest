59db019a0576daa93ba3ba8d06ae1a6b5c030713
quocbao1492 <baottq.uit@gmail.com>
2016-06-28 16:29:35 +0700

    file changed

--- GithubTest/Test.py
+++ GithubTest/Test.py
@@ -4,7 +4,13 @@ import time
 driver = webdriver.Chrome()
 
 driver.get("http://tinhte.vn")
+
+print "get website"
+
 driver.find_element_by_xpath("//*[@id='navigation']/div/div/div/nav/div/div/ul[2]/li[1]/a").click()
 time.sleep(4)
+print "click Login"
 
 driver.quit()
+
+print  "has quit"
