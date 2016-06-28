18f13f0926ef024dd12d8875f11a9f4aef513762
quocbao1492 <baottq.uit@gmail.com>
2016-06-28 15:38:52 +0700

    changed

--- GithubTest/Test.py
+++ GithubTest/Test.py
@@ -1,13 +1,10 @@
-# from selenium import webdriver
+from selenium import webdriver
 import time
 
-# driver = webdriver.Chrome(executable_path="C:\Test\GithubTest\chromedriver.exe")
-#
-# driver.get("http://tinhte.vn")
-# driver.find_element_by_xpath("//*[@id='navigation']/div/div/div/nav/div/div/ul[2]/li[1]/a").click()
-# time.sleep(4)
-#
-# driver.quit()
+driver = webdriver.Chrome(executable_path="C:\Test\GithubTest\chromedriver.exe")
 
+driver.get("http://tinhte.vn")
+driver.find_element_by_xpath("//*[@id='navigation']/div/div/div/nav/div/div/ul[2]/li[1]/a").click()
+time.sleep(4)
 
-print "aaihai"
+driver.quit()
